6f97122dfb72c7f6c7c0e2c00e4a60fa6a808058
igor <itxn@mail.ru>
2022-09-15 01:12:56 +0100

    Task 2_6;

---
+++ src/test/java/Task2ex6.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Task2ex6 {
+
+    @Test
+    public void redirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String redirectLocationHeader = response.getHeader("Location");
+        System.out.println(redirectLocationHeader);
+    }
+}
